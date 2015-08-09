bec9797afca13e5577b65eb7c62e500e55fa6c1c
Domingo Suarez Torres <domingo.suarez@gmail.com>
2015-08-08 23:15:29 -0500

    added the Health Indicator

--- clase-1/demo/src/main/groovy/demo/indicator/MyHealthIndicator.groovy
+++ clase-1/demo/src/main/groovy/demo/indicator/MyHealthIndicator.groovy
@@ -2,10 +2,12 @@ package demo.indicator
 
 import org.springframework.boot.actuate.health.AbstractHealthIndicator
 import org.springframework.boot.actuate.health.Health
+import org.springframework.stereotype.Component
 
 /**
  * Created by domix on 08/08/15.
  */
+@Component
 class MyHealthIndicator extends AbstractHealthIndicator {
   @Override
   protected void doHealthCheck(Health.Builder builder) throws Exception {
